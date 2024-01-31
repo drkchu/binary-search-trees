dc52087d586803f8d011f2c3d8c3efa2a89c816d
drkchu <derekdbchu@gmail.com>
2024-01-31 11:28:11 -0800

    Add instructions for tomorrow

---
+++ driver-script.js
@@ -0,0 +1,8 @@
+// Create a binary search tree from an array of random numbers < 100. You can create a function that returns an array of random numbers every time you call it if you wish.
+// Confirm that the tree is balanced by calling isBalanced.
+// Print out all elements in level, pre, post, and in order.
+// Unbalance the tree by adding several numbers > 100.
+// Confirm that the tree is unbalanced by calling isBalanced.
+// Balance the tree by calling rebalance.
+// Confirm that the tree is balanced by calling isBalanced.
+// Print out all elements in level, pre, post, and in order.
